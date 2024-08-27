c270cb8eb01d50f1a8f7cfc42dd9950211b24c72
TrashUwU <83889502+WolfGamer2@users.noreply.github.com>
2024-08-27 09:20:23 -0700

    Update SignUpLink.tsx
    Added Zenith 2025

--- src/features/Login/SignUpLink.tsx
+++ src/features/Login/SignUpLink.tsx
@@ -8,7 +8,7 @@ const SignUpLink: React.FC<{}> = (props) => {
     <div className="SignUpLink" style={{ fontFamily: theme.fonts.header }}>
       Don't have an account? &nbsp;
       <LinkDuo to={FrontendRoute.CREATE_ACCOUNT_PAGE}>
-        <span>Sign up</span>
+        <span>Sign up for Zenith 2025</span>
       </LinkDuo>
     </div>
   );
